99049e229846877b38a85c7b5b5f1d2743707ebf
gourab mukherjee <gourab.mukherjee@sap.com>
2021-06-19 08:30:26 +0000

    updated

--- sameln.sh
+++ sameln.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $# != 1 ]
 then
-	echo "ERROR! Improper Argument List"
-	exit 1
+    echo "ERROR! Improper Argument List"
+    exit 1
 else
     if [ -d $1 ]
     then
@@ -18,10 +18,18 @@ else
         # stores unique hash values 
         dup_hash_all=($(cat mdresult | sort | uniq -w32 -d | cut -b 1-32))
         for ((i = 0 ; i < ${#dup_hash_all[*]} ; i++)); do
-            cat mdresult | grep "${dup_hash_all[i]}" | sort #| head -n 1 | cut -b 35-
-            ########################################################
+           cat mdresult | grep "${dup_hash_all[i]}" | sort | cut -d " " -f 3- | tee allFiles
+           #cut -c 2- allFiles | tee allFiles
+           mapfile allFilesArray < allFiles
+           for((j = 1; j < ${#allFilesArray[@]}; j++))
+           do
+             allFilesArray[j]=$(echo "${allFilesArray[j]}" | tr -d '\n')
+             #echo "${allFilesArray[j]}"
+             ln "${allFilesArray[0]}" "${allFilesArray[j]}" 
+             echo "${allFilesArray[j]} has been hard linked to ${allFilesArray[0]}"
+           done
         done
-        rm mdresult
+       rm mdresult
     else
         echo "Directory does not exist"
     fi
