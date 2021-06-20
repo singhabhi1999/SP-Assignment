f715aa8c025e68c541bf0ff22a437c2745a59572
gourab mukherjee <gourab.mukherjee@sap.com>
2021-06-20 14:56:31 +0000

    readme part2 added

--- sameln.sh
+++ sameln.sh
@@ -18,17 +18,22 @@ else
         dup_hash_all=($(cat mdresult | sort | uniq -w32 -d | cut -b 1-32))
         for ((i = 0 ; i < ${#dup_hash_all[*]} ; i++)); do
            cat mdresult | grep "${dup_hash_all[i]}" | sort | cut -d " " -f 3- > allFiles
-           #cut -c 2- allFiles | tee allFiles
+           # convert contents of allFiles file into array
            mapfile allFilesArray < allFiles
+           # trim unwanted newline character from the end.
            allFilesArray[0]=$(echo "${allFilesArray[0]}" | tr -d '\n')
            for((j = 1; j < ${#allFilesArray[@]}; j++))
            do
+             # trim unwanted newline character from the end.
              allFilesArray[j]=$(echo "${allFilesArray[j]}" | tr -d '\n')
+             # remove the duplicate file
              rm "${allFilesArray[j]}"
+             # create hardlink
              ln "${allFilesArray[0]}" "${allFilesArray[j]}" 
              echo "${allFilesArray[j]} has been hard linked to ${allFilesArray[0]}"
            done
         done
+        # removing extra files.
        rm mdresult
        rm allFiles
     else
